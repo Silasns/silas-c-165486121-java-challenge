f3a156799107d379e20bb14875c3caa4705109e1
Silas Cardoso <silasnunes105@gmail.com>
2025-03-03 12:36:49 -0300

    Commit inicial

---
+++ src/test/java/com/loja_virtual_api/LojaVirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja_virtual_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
